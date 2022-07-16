fc14fe60349c715228e7ccef9d4722eb5374d9e0
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2022-07-16 17:27:24 +0200

    Add places and people placeholders

--- scrappers/spanish.py
+++ scrappers/spanish.py
@@ -12,9 +12,23 @@ class SpanishScrapper:
   def get_verbs(self):
     return []
 
+  def get_places(self):
+    return []
+
+  def get_people(self):
+    return []
+
   def get_words(self):
     nouns = self.get_nouns()
     adjectives = self.get_adjectives()
     verbs = self.get_verbs()
+    places = self.get_places()
+    people = self.get_people()
 
-    return { 'nouns': nouns, 'adjectives': adjectives, 'verbs': verbs }
+    return {
+      'nouns': nouns,
+      'adjectives': adjectives,
+      'verbs': verbs,
+      'places': places,
+      'people': people
+    }
